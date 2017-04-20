8955f4050d354235409d6e80575f12f4cae69511
amody <amtmody@yahoo.com>
2017-04-20 17:43:29 -0400

    All files message

---
+++ test/my-app/src/main/java/com/iconectiv/app/App.java
@@ -0,0 +1,13 @@
+package com.iconectiv.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Amit!" );
+    }
+}
